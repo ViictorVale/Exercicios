bf02926d7cd72b5913290b05ef074fdd8f297ab3
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-11 15:48:44 -0300

    Exercicio estrutura for

---
+++ Exercicio estrutura for/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio_estrutura_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                soma = soma + x;
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}

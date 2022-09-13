f75b6e4274181fb4094171d0a26feb544d5527fd
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-12 21:24:20 -0300

    Exercicio faça enquanto

---
+++ Exercicio faca enquanto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio_faca_enquanto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char condicao;
+            do
+            {
+                Console.Write("Digite a temperatura em Celsius: ");
+                double C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                double F = 9 * C / 5 + 32;
+                Console.WriteLine("Equivalente em Fahrenheit: " + F.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("Deseja repetir (s/n)? ");
+                condicao = char.Parse(Console.ReadLine());
+            } 
+            while (condicao == 's');
+
+            Console.ReadLine();
+        }
+    }
+}

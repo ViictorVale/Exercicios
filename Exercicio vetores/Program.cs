fd2249603d98515c20bdba2ff75d8101b7f2394a
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-19 17:59:57 -0300

    Exercicios

---
+++ Exercicio vetores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vet;
+            int n = int.Parse(Console.ReadLine());
+            vet = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(vet[i].ToString("F1", CultureInfo.InvariantCulture));
+            }
+
+        }
+    }
+}

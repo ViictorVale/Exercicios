5aad16bbea9849f0cf3f772dd70577d227952a80
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-19 22:10:44 -0300

    Exercícios

---
+++ Exercio 2 vetores/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercio_2_vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet;
+            int n = int.Parse(Console.ReadLine());
+            vet = new int[n];
+
+            string[] linha = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = int.Parse(linha[i]);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if(vet[i] < 0)
+                {
+                    Console.WriteLine(vet[i]);
+                }
+            }
+        }
+    }
+}

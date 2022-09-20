96ac41b0c5678df718223f9ca74057e281b8ec2d
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-20 10:36:04 -0300

    Exercícios

---
+++ Exercicio 3 vetores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio_3_vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] a;
+            int n = int.Parse(Console.ReadLine());
+            a = new double [n];
+
+            string[] linha = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = double.Parse(linha[i], CultureInfo.InvariantCulture);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i].ToString("F1", CultureInfo.InvariantCulture) + " ");
+            }
+            Console.WriteLine();
+            double soma = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                soma = soma + a[i];
+            }
+            double media = soma / n;
+            Console.WriteLine(soma.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}

2c915f73bb94d68e1847b9259d4853082b8333bf
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-20 19:54:58 -0300

    Exercícios

---
+++ Exercicio 6 vetores/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Exercicio_6_vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] num = new int[n];
+            string[] linha = Console.ReadLine().Split(' ');
+
+            for(int i = 0; i < n; i++)
+            {
+                num[i] = int.Parse(linha[i]);
+            }
+
+            // Mostrar todos os números pares
+            int numpares = 0;
+            int cont = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (num[i] % 2 == 0)
+                {
+                    numpares = num[i];
+                    cont++;
+                    Console.Write(numpares + " ");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(cont);
+        }
+    }
+}

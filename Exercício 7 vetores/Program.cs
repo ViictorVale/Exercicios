b3a3629d7a0858e02beb378f2b31ecb5b7fa5a14
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-20 20:28:17 -0300

    Exercícios

---
+++ Exercício 7 vetores/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercício_7_vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            int[] b = new int[n];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for(int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(s[i]);
+            }
+
+            s = Console.ReadLine().Split(' ');
+            for(int i = 0; i < n; i++)
+            {
+                b[i] = int.Parse(s[i]);
+            }
+
+            int[] c = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                c[i] = a[i] + b[i];
+            }
+            
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write(c[i] + " ");
+            }
+        }
+    }
+}

1d7f01dcf83258399a1983766788b983ceb45865
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-21 20:25:25 -0300

    Exercícios

---
+++ Exercício 11 vetores/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+namespace Exercício_11_vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] nomes = new string[n];
+            double[] nota1 = new double[n];
+            double[] nota2 = new double[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                nomes[i] = (s[0]);
+                nota1[i] = double.Parse(s[1], CultureInfo.InvariantCulture);
+                nota2[i] = double.Parse(s[2], CultureInfo.InvariantCulture);
+            }
+            Console.WriteLine("Alunos aprovados:");
+           
+            for (int i = 0; i < n; i++)
+            {
+               double media = (nota1[i] + nota2[i]) / 2.0;
+                if(media >= 6.0)
+                {
+                    Console.WriteLine(nomes[i]);
+                }
+            }
+        }
+    }
+}

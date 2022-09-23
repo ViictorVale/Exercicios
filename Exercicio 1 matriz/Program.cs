7d3cfc60cfc1e6c613321909e0c49bb26bcde8a5
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-22 21:19:55 -0300

    Exercícios

---
+++ Exercicio 1 matriz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercicio_1_matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int m = int.Parse(s[0]);
+            int n = int.Parse(s[1]);
+            int[,] a = new int[m, n];
+            
+            for(int i = 0; i < m; i++)
+            {
+                string[] s1 = Console.ReadLine().Split(' ');
+                for(int j = 0; j < n; j++)
+                {
+                    a[i, j] = int.Parse(s1[j]);
+                }
+            }
+
+            for(int i = 0; i < m; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+           
+        }
+    }
+}

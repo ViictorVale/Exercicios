ee79dbd7713e0948df28284328b51a628d652ea6
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-25 18:33:44 -0300

    Exercícios

---
+++ Exercicio 9 matriz/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio_9_matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double[,] mat = new double[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = double.Parse(s[j], CultureInfo.InvariantCulture);
+                }
+            }
+
+            int linha = int.Parse(Console.ReadLine());
+            int coluna = int.Parse(Console.ReadLine());
+
+            //soma dos positivos
+
+            double soma = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] > 0.0)
+                    {
+                        soma = soma + mat[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("SOMA DOS POSITIVOS: " + soma);
+
+            //linha escolhida
+            Console.Write("LINHA ESCOLHIDA: ");
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(mat[linha, j].ToString("F1", CultureInfo.InvariantCulture) + " ");
+            }
+            Console.WriteLine();
+
+            //coluna escolhida
+            Console.Write("COLUNA ESCOLHIDA: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, coluna].ToString("F1", CultureInfo.InvariantCulture) + " ");
+            }
+            Console.WriteLine();
+
+            //diagonal principal
+            Console.Write("DIAGONAL PRINCIPAL: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i].ToString("F1", CultureInfo.InvariantCulture) + " ");
+            }
+            Console.WriteLine();
+
+            //matriz alterada
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0.0)
+                    {
+                        mat[i, j] = mat[i, j] * mat[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("MATRIZ ALTERADA:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(mat[i, j].ToString("F1", CultureInfo.InvariantCulture) + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}

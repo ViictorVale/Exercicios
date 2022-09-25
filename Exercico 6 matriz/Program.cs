3dd58c655507f4c657c312a97f529e612980a03a
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-25 16:28:28 -0300

    Exercícios

---
+++ Exercico 6 matriz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercico_6_matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n, n];
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for(int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int maior = mat[i, 0];
+                for (int j = 0; j < n; j++)
+                {
+                    if(mat[i, j] > maior)
+                    {
+                        maior = mat[i, j];
+                    }
+                }
+                Console.WriteLine(maior);
+            }
+        }
+    }
+}

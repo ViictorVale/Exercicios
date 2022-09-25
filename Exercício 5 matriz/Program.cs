279bf2cd1fe741eb911de1edf60380f34c58a59d
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-25 15:59:14 -0300

    Exercícios

---
+++ Exercício 5 matriz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercício_5_matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n, n];
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(s[j]);
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int soma = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    soma = soma + mat[i, j];
+                }
+                Console.WriteLine(soma);
+            }
+        }
+    }
+}

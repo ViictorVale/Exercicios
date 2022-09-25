dbe136b990d7a2b5b712d46299debfca17a1589f
ViictorVale <105989314+ViictorVale@users.noreply.github.com>
2022-09-25 15:51:50 -0300

    Exercício

---
+++ Exercicio 4 matriz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercicio_4_matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s1 = Console.ReadLine().Split(' ');
+            int m = int.Parse(s1[0]);
+            int n = int.Parse(s1[1]);
+            int[,] mat = new int[m, n];
+
+            for(int i = 0; i < m; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for(int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            Console.WriteLine("VALORES NEGATIVOS: ");
+            for(int i = 0; i < m; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    if(mat[i,j] < 0)
+                    {
+                        Console.WriteLine(mat[i, j]);
+                    }
+                }
+            }
+        }
+    }
+}
